b33d08d44337fca26c55c28472b02240164a457c
root <you@example.com>
2026-06-17 02:23:29 +0000

    --no-check-certificate

--- navsim/download/download_navtrain_aws.sh
+++ navsim/download/download_navtrain_aws.sh
@@ -1,4 +1,4 @@
-wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/openscene-v1.1/openscene_metadata_trainval.tgz
+wget --no-check-certificate https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/openscene-v1.1/openscene_metadata_trainval.tgz
 tar -xzf openscene_metadata_trainval.tgz
 rm openscene_metadata_trainval.tgz
 mv openscene-v1.1/meta_datas trainval_navsim_logs
@@ -6,7 +6,7 @@ rm -r openscene-v1.1
 
 mkdir -p trainval_sensor_blobs/trainval
 for split in {1..4}; do
-    wget https://s3.eu-central-1.amazonaws.com/avg-projects-2/navsim/navtrain_current_${split}.tgz
+    wget --no-check-certificate https://s3.eu-central-1.amazonaws.com/avg-projects-2/navsim/navtrain_current_${split}.tgz
     echo "Extracting file navtrain_current_${split}.tgz"
     tar -xzf navtrain_current_${split}.tgz
     rm navtrain_current_${split}.tgz
@@ -16,7 +16,7 @@ for split in {1..4}; do
 done
 
 for split in {1..4}; do
-    wget https://s3.eu-central-1.amazonaws.com/avg-projects-2/navsim/navtrain_history_${split}.tgz
+    wget --no-check-certificate https://s3.eu-central-1.amazonaws.com/avg-projects-2/navsim/navtrain_history_${split}.tgz
     echo "Extracting file navtrain_history_${split}.tgz"
     tar -xzf navtrain_history_${split}.tgz
     rm navtrain_history_${split}.tgz
